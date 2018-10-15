a122d2db1100f57507c76956d297848312ab240b
Grzegorz Stochel <danielstochel332@gmail.com>
2018-10-15 19:20:28 +0200

    initial commit

---
+++ src/api/todoApi.js
@@ -0,0 +1,37 @@
+import {HttpClient} from './httpClient'
+
+// This is the API. The backend root URL can be set from here.
+
+const API = 'https://todos-api-learn.herokuapp.com/todos'
+
+//Setting the todos URI
+
+// The CRUD Operations of the Todo Resource.
+
+
+//Create
+const createTodo = todo => {
+    return HttpClient.post(API, todo)
+}
+
+//Read
+const getTodo = () => {
+    return HttpClient.get(API)
+}
+
+//Update
+const updateTodo = todo => {
+    return HttpClient.put(API, todo)
+}
+
+//Delete
+const removeTodo = todo => {
+    return HttpClient.delete(`${API}/${todo._id}`)
+}
+
+
+//Encapsulating in a JSON object
+
+const TodoApi = {createTodo, getTodo, updateTodo, removeTodo}
+
+export {TodoApi}
---
+++ src/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { createStore, applyMiddleware } from 'redux';
+import { configureStore } from './store/configureStore'
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux'
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+import * as TodoActions from './actions/todoActions'
+
+const store = configureStore()
+
+store.dispatch(TodoActions.GetTodos())
+
+ReactDOM.render(
+  <Provider store={store} >
+    <App />
+</Provider>, document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.unregister();
