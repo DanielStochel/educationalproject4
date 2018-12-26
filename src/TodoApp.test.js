ddfc4c8cb48e3b11e6413280bedcebafa9933616
Grzegorz Stochel <danielstochel332@gmail.com>
2018-12-26 18:56:36 +0100

    test- learning

--- src/TodoApp.test.js
+++ src/TodoApp.test.js
@@ -2,7 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import TodoApp from "./TodoApp";
 import { shallow } from "enzyme";
+import ErrorsContainer from "./container/ErrorsContainer";
 
-it("renders without crashing", () => {
-  shallow(<TodoApp />);
+it("renders welcome message", () => {
+  const wrapper = shallow(<TodoApp />);
+  const welcome = <ErrorsContainer />;
+  // expect(wrapper.contains(welcome)).toBe(true);
+  expect(wrapper.contains(welcome)).toEqual(true);
 });
