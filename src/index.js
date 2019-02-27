5eae9ab617e21b4990d47f45e69acf3c085c814b
Grzegorz Stochel <danielstochel332@gmail.com>
2019-02-27 21:21:23 +0100

    delete spaces

--- src/index.js
+++ src/index.js
@@ -1,21 +1,23 @@
-import React from 'react';
-import { configureStore } from './store/configureStore'
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux'
-import './index.css';
-import 'semantic-ui-css/semantic.min.css';
-import 'font-awesome/css/font-awesome.css'
-import TodoApp from './TodoApp';
-import * as serviceWorker from './serviceWorker';
-import * as TodoActions from './actions/todoActions'
+import React from "react";
+import { configureStore } from "./store/configureStore";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import "./index.css";
+import "semantic-ui-css/semantic.min.css";
+import "font-awesome/css/font-awesome.css";
+import TodoApp from "./TodoApp";
+import * as serviceWorker from "./serviceWorker";
+import * as TodoActions from "./actions/todoActions";
 
-const store = configureStore()
+const store = configureStore();
 
-store.dispatch(TodoActions.GetTodos())
+store.dispatch(TodoActions.GetTodos());
 
 ReactDOM.render(
   <Provider store={store}>
     <TodoApp />
-  </Provider>, document.getElementById('root'));
+  </Provider>,
+  document.getElementById("root")
+);
 
 serviceWorker.unregister();
