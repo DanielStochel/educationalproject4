3725462a4ed3342e4931881eb9e2a9f21c09877d
Grzegorz Stochel <danielstochel332@gmail.com>
2019-04-17 16:54:09 +0200

    change in jest testing

--- src/components/componentsTesting/options.test.js
+++ src/components/componentsTesting/options.test.js
@@ -1,22 +1,21 @@
 import React from "react";
-import { render } from "enzyme";
 import Options from "../Options";
-import {AddOptions, EditOptions} from "../Options";
+import { AddOptions, EditOptions } from "../Options";
 import ShallowRenderer from "react-test-renderer/shallow";
 
 describe("checking the render <Options />", () => {
   it("test if component gets incorrect props", () => {
     const renderer = new ShallowRenderer();
     const renderOptions = renderer.render(<Options />);
-    const renderAddOptions = renderer.render(<AddOptions />)
-    
+    const renderAddOptions = renderer.render(<AddOptions />);
+
     return expect(renderOptions).toEqual(renderAddOptions);
-    })
+  });
   it("test if component gets correct props", () => {
     const renderer = new ShallowRenderer();
-    const renderOptions = renderer.render(<Options todo={{editing: true}} />);
+    const renderOptions = renderer.render(<Options todo={{ editing: true }} />);
     const renderEditOptions = renderer.render(<EditOptions />);
 
     return expect(renderOptions).toEqual(renderEditOptions);
-  })
+  });
 });
